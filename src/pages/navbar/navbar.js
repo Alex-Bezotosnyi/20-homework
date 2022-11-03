71bc1c2ca2f01b4d7043760fca9891bd958fac66
Alex-Bezotosnyi <110969172+Alex-Bezotosnyi@users.noreply.github.com>
2022-11-03 11:16:09 +0300

    renew Routes

--- src/pages/navbar/navbar.js
+++ src/pages/navbar/navbar.js
@@ -5,8 +5,8 @@ import navbarCSS from "./style/navbar.module.css"
 const Navbar = (props) => {
     return (
         <nav className={navbarCSS.nav}>
-            <NavLink to="/" className={navbarCSS.title_wrapper}>Homework</NavLink>
             <ul>
+                <NavLink to="/" className={navbarCSS.title_wrapper}>Home</NavLink>
                 <NavLink to="/src/pages/posts/main-posts">Posts</NavLink>
                 <NavLink to="/gallery">Gallery</NavLink>
                 <NavLink to="/src/pages/contacts/">Contacts</NavLink>
